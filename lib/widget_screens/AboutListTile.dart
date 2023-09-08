aad5b056d1a8377707abf20891111ecdd9c207d9
hung1720 <hungtdqse140575@fpt.edu.vn>
2023-09-09 01:20:14 +0700

    widget 1

---
+++ lib/widget_screens/AboutListTile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyApp1 extends StatelessWidget {
+  const MyApp1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: null,
+            ),
+          ),
+          SizedBox(
+            width: 100.0,
+            height: 200.0,
+            child: ElevatedButton(
+              // AbsorbPointer
+              style: ElevatedButton.styleFrom(primary: Colors.blue.shade200),
+              onPressed: () {},
+              child: null,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
